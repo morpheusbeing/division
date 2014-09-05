c97d0fddc16ebb6f795f850b4a9a2784f91670dd
Morpheus Being <morpheus.being@riseup.net>
2014-09-06 09:35:05 +1000

    Separated IO and split into functions

--- Division.hs
+++ Division.hs
@@ -1,22 +1,40 @@
 {- Division.hs - provide check answers for kids long division exercises -}
 
-
-main :: IO ()
-main = do
+{- get 2 integers from keyboard -}
+getInts :: IO (Int, Int)
+getInts = do
    putStrLn "Please enter the dividend :"
-   inputx  <- getLine
-   putStrLn "Please enter the divisor :"
-   inputy  <- getLine
+   x <- readLn
+   putStrLn "Please enter the divisor  :"
+   y <- readLn
+   return (x, y)
    
-   let x = (read inputx) :: Int
-   let y = (read inputy) :: Int
-   let z = x `div` y
-   let remain = x `mod` y
+{- do the calculations -}
+sums :: (Int, Int) -> [Int]
+sums (x, y) = [x, y, q, r, y * q, y * q + r] where
+              q = x `div` y
+              r = x `mod` y
+
+{- create the explanation output with the results -}
+explain :: [String] -> [String]
+explain [x,y,q,r,yq,yq_r] = 
+   [ concat ["Result: ", x, " / ", y, " = ", q, " remainder ", r]
+   , concat ["Proof : (", y, " x ", q, ") + ", r, " = ", yq, " + ",r, " = ", yq_r]
+   , "Is this what you had? "]
    
-   let result = "Result: " ++ show x ++ " / " ++ show y ++ " =  " ++ show z ++  " remainder " ++ show remain
-   let proof =  "Proof: (" ++ show y ++ " x " ++ show z ++ ") = " ++ show (y * z) ++ " + " ++ show remain ++ " = " ++ show ((y * z) + remain)
-   
-   putStrLn result
-   putStrLn proof
-   putStrLn ( "Is this what you had? ")
+{- simpler main -}
+{- rewritten main to use the developed functions 
+main = do (x,y) <- getInts
+          let ns = map show ( sums (x,y) )
+              es = explain ns
+          mapM_ putStrLn es
+-}
+
+{- most succint main -}
+{- This main pipes the functions explain . map show . sums 
+ - and applies that to the output of getInts using fmap
+ -}
+main :: IO ()
+main = fmap (explain . map show . sums) getInts
+       >>= mapM_ putStrLn
    
